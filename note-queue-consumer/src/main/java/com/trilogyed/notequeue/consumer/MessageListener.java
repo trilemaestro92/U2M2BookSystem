e50d93598b6c28bcd2bc3d94332deaf142823446
Arturo Salmeron <asalmeron4@dhcp-10-101-246-34.wireless.northwestern.private>
2019-07-03 13:03:42 -0500

    Drafting note consumer setup. Listener is currently broke

--- note-queue-consumer/src/main/java/com/trilogyed/notequeue/consumer/MessageListener.java
+++ note-queue-consumer/src/main/java/com/trilogyed/notequeue/consumer/MessageListener.java
@@ -1,7 +1,9 @@
 package com.trilogyed.notequeue.consumer;
 
+import com.trilogyed.notequeue.consumer.util.feign.NoteClient;
 import com.trilogyed.notequeue.consumer.util.messages.NoteListEntry;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,12 +11,12 @@ public class MessageListener {
 
     // This listener should be for adding note
     @RabbitListener(queues = NoteQueueConsumerApplication.QUEUE_NAME)
-    public void receiveMessage(NoteListEntry msg) {
-        // Send note to the note microservice
+    public String receiveMessage(NoteListEntry msg) {
+        System.out.println(msg.toString());
+        return "note queue listener";
 
     }
 
 
-    // Another listener should be for updating a note
 
 }

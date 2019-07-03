a190220aa6a458a51210d0dce9e7fea1166035e1
Arturo Salmeron <asalmeron4@Arturos-MBP.attlocal.net>
2019-07-02 22:35:10 -0500

    NotesDao & JdbcTemplateImp added. NoteDao tests passing

--- note-service/src/main/java/com/trilogyed/noteservice/dao/NoteDao.java
+++ note-service/src/main/java/com/trilogyed/noteservice/dao/NoteDao.java
@@ -1,4 +1,14 @@
 package com.trilogyed.noteservice.dao;
 
+import com.trilogyed.noteservice.model.Note;
+
+import java.util.List;
+
 public interface NoteDao {
+    public Note createNote(Note note);
+    public Note getNote(int note_id);
+    public List<Note> getNotesByBookId(int bookId);
+    public List<Note> getAllNotes();
+    public String updateBook(Note note);
+    public String deleteBook(int id);
 }
--- note-service/src/main/java/com/trilogyed/noteservice/model/Note.java
+++ note-service/src/main/java/com/trilogyed/noteservice/model/Note.java
@@ -1,4 +1,71 @@
 package com.trilogyed.noteservice.model;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import java.util.Objects;
+
 public class Note {
+
+    private int noteId;
+    @NotNull(message = "bookId can not be null")
+    private int bookId;
+    @Size(max = 255, message = "note cannot exceed 255 characters")
+    private String note;
+
+    public Note() {
+    }
+
+    public Note(int bookId, String note) {
+        this.bookId = bookId;
+        this.note = note;
+    }
+
+    public int getNoteId() {
+        return noteId;
+    }
+
+    public void setNoteId(int noteId) {
+        this.noteId = noteId;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Note note1 = (Note) o;
+        return noteId == note1.noteId &&
+                bookId == note1.bookId &&
+                Objects.equals(note, note1.note);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(noteId, bookId, note);
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "noteId=" + noteId +
+                ", bookId=" + bookId +
+                ", note='" + note + '\'' +
+                '}';
+    }
 }
